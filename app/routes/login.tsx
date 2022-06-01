fb22ef9f277867f70c2acfc44b2f4221bf58864b
Kent C. Dodds <me+github@kentcdodds.com>
2022-06-01 01:16:11 -0400

    don't need this

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,7 +5,7 @@ import type {
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import { Form, useActionData, useSearchParams } from "@remix-run/react";
 import * as React from "react";
 
 import { createUserSession, getUserId } from "~/session.server";
